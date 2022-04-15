8787f2425356a9b43f4f41af3cb0b73985ae1435
jkr02 <jakubkroczek02@gmail.com>
2022-04-15 22:32:08 +0200

    commit

--- algorytmy dynamiczne/nawiasowanie__macierzy.py
+++ algorytmy dynamiczne/nawiasowanie__macierzy.py
@@ -1,15 +1,16 @@
-def NM(lista):
-    n=len(lista)
+def NM(p):
+    n=len(p)
     m = [[0 for _ in range(n)] for _ in range(n)]
-    s = [[0 for _ in range(n)] for _ in range(n)]
     for i in range(1, n):
-        for j in range(n-i):
-            l=j+i
-            if j<l-2:
-                m[j][l]
-            for k in range(j, l-1):
-                q=m[j][k]+m[k+1][j]+(len(lista[j-1])*len(lista[k])*len(lista[l]))
-                if q<m[j][l]:
-                    m[j][l]=q
-                    s[j][l]=k
-    return m, s
+        m[i][i] = 0
+    for L in range(2, n):
+        for i in range(1, n - L + 1):
+            j = i + L - 1
+            m[i][j] = m[i][i] + m[i + 1][j] + p[i - 1] * p[i] * p[j]
+            for k in range(i+1, j):
+                q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j]
+                if q < m[i][j]:
+                    m[i][j] = q
+    return m[1][n - 1]
+arr = [1, 2, 3, 4]
+print(NM(arr))
