8f1f6483d4655bf86b94871b301011ec85e75ce9
jkr02 <jakubkroczek02@gmail.com>
2022-04-22 23:43:49 +0200

    commit

--- offline4/zad4.py
+++ offline4/zad4.py
@@ -10,19 +10,22 @@ def select_buildings(T,p):
                 if T[x][1] > T[x + 1][1]:
                     T[x], T[x + 1] = T[x + 1], T[x]
                     indeksy[x], indeksy[x+1] = indeksy[x+1], indeksy[x]
+                elif T[x][1] >= T[x + 1][1] and T[x][2]<T[x+1][2]:
+                    T[x], T[x + 1] = T[x + 1], T[x]
+                    indeksy[x], indeksy[x + 1] = indeksy[x + 1], indeksy[x]
         return T, indeksy
     P, indeksy=Bubble_sort(T)
-    tab = [[[0] for _ in range(p+1)] for _ in range(n)]
+    tab = [[[0] for _ in range(p+1)] for _ in range(n+1)]
     for i in range(n):
         for j in range(p+1):
             if j>=P[i][3]:
-                max_k=0
+                max_k=n
                 max_sum=0
                 for k in range(i-1, -1, -1):
-                    if P[tab[k][j-P[i][3]][-1]][2]<P[i][1] and tab[k][j-P[i][3]][0]>=max_sum:
-                        max_k=k
-                        max_sum=tab[k][j-P[i][3]][0]
-
+                    if len(tab[k][j-P[i][3]])>1:
+                        if P[tab[k][j-P[i][3]][-1]][2]<P[i][1] and tab[k][j-P[i][3]][0]>max_sum:
+                            max_k=k
+                            max_sum=tab[k][j-P[i][3]][0]
                 if max_sum+(P[i][2]-P[i][1])*P[i][0]>tab[i-1][j][0]:
                     tab[i][j]=tab[max_k][j-P[i][3]][:]
                     tab[i][j].append(i)
