684a7356b9c268972c33c02ef0306274a1cb3480
jkr02 <jakubkroczek02@gmail.com>
2022-05-05 23:19:50 +0200

    commit

---
+++ cwiczenia9/sieci.py
@@ -0,0 +1,2 @@
+# operator sieci komorkowej konczy dzialalnosc, stacje telefonii są grafem spójnym, wyłączamy pojedyńczo tak żeby
+# pozostały graf dalej byl spojny
--- offline6/zad6.py
+++ offline6/zad6.py
@@ -3,8 +3,6 @@ import collections
 from zad6testy import runtests
 
 def longer( G, s, t ):
-    tab=[]
-    tab1=[]
     Q = collections.deque()
     class wierzcholek:
         def __init__(self):
@@ -22,27 +20,24 @@ def longer( G, s, t ):
                 wierzcholki[G[u][i]].d=wierzcholki[u].d+1
                 wierzcholki[G[u][i]].parent=u
                 Q.append(G[u][i])
-            elif G[u][i]==t and wierzcholki[u].d==wierzcholki[t].d-1:
-                wierzcholki.append(wierzcholek())
-                wierzcholki[-1].visited=True
-                wierzcholki[-1].d=wierzcholki[u].d+1
-                wierzcholki[-1].parent=u
-    x=t
-    while wierzcholki[x].parent!=None:
-        tab.append([x, wierzcholki[x].parent])
-        x=wierzcholki[x].parent
-    for i in range(len(G), len(wierzcholki)):
-        if [t, wierzcholki[i].parent] in tab:
-            tab1.append([t, wierzcholki[i].parent])
-        x = wierzcholki[i].parent
-        while wierzcholki[x].parent!=None:
-            if [x, wierzcholki[x].parent] in tab:
-                tab1.append([x, wierzcholki[x].parent])
-            x = wierzcholki[x].parent
-        tab = tab1.copy()
-        tab1 = []
-    if len(tab)==0:
-        return None
-    return tab[0]
+    pop=[t]
+    tmp=[]
+    for i in range(wierzcholki[t].d, 0, -1):
+        if len(pop) == 1:
+            for x in range(len(G[pop[0]])):
+                if wierzcholki[G[pop[0]][x]].d == i-1:
+                    if not G[pop[0]][x] in tmp:
+                        tmp.append(G[pop[0]][x])
+            if len(tmp) == 1:
+                return (pop[0], tmp[0])
+        else:
+            for j in range(len(pop)):
+                for x in range(len(G[pop[j]])):
+                    if wierzcholki[G[pop[j]][x]].d == i-1:
+                        if not G[pop[j]][x] in tmp:
+                            tmp.append(G[pop[j]][x])
+        pop=tmp.copy()
+        tmp=[]
+    return None
 # zmien all_tests na True zeby uruchomic wszystkie testy
 runtests( longer, all_tests = True )
